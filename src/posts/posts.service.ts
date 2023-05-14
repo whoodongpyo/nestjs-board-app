ca92743f498e080553dadef4e3db21903e8d9c12
whoodongpyo <vvhooping@gmail.com>
2023-05-14 18:43:59 +0900

    feat: 게시글 생성 API 추가

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Post } from './post.model';
+import { Post, PostStatus } from './post.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class PostsService {
@@ -7,5 +8,17 @@ export class PostsService {
 
   getAllPosts(): Post[] {
     return this.posts;
+  }
+
+  createPost(title: string, description: string) {
+    const post: Post = {
+      id: uuid(),
+      title,
+      description,
+      status: PostStatus.PUBLIC,
+    };
+
+    this.posts.push(post);
+    return post; // 생성한 게시글 정보를 확인용으로 전달하기
   }
 }
