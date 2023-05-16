52b7d23f1f5e89e3bc0fbadf44143f701d325acb
whoodongpyo <vvhooping@gmail.com>
2023-05-16 23:54:30 +0900

    chore: 주석 제거

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -6,39 +6,8 @@ import { Post } from './post.entity';
 export class PostsController {
   constructor(private postsService: PostsService) {}
 
-  //
-  // @Get('/')
-  // getAllPosts(): iPost[] {
-  //   return this.postsService.getAllPosts();
-  // }
-  //
-  // @Post('/')
-  // @UsePipes(ValidationPipe)
-  // createPost(@Body() createPostDto: CreatePostDto): iPost {
-  //   return this.postsService.createPost(createPostDto);
-  // }
-  //
-
   @Get('/:id')
   getPostById(@Param('id') id: number): Promise<Post> {
     return this.postsService.getPostById(id);
   }
-
-  // @Get('/:id')
-  // getPostById(@Param('id') id: string): iPost {
-  //   return this.postsService.getPostById(id);
-  // }
-  //
-  // @Delete('/:id')
-  // deletePostById(@Param('id') id: string): iDeletePostResponse {
-  //   return this.postsService.deletePostById(id);
-  // }
-  //
-  // @Patch('/:id/status')
-  // updatePostStatus(
-  //   @Param('id') id: string,
-  //   @Body('status', PostStatusValidationPipe) status: PostStatus,
-  // ) {
-  //   return this.postsService.updatePostStatus(id, status);
-  // }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -6,26 +6,6 @@ import { Post } from './post.entity';
 export class PostsService {
   constructor(private postRepository: PostRepository) {}
 
-  // private posts: iPost[] = [];
-  //
-  // getAllPosts(): iPost[] {
-  //   return this.posts;
-  // }
-  //
-  // createPost(createPostDto: CreatePostDto) {
-  //   const { title, description } = createPostDto;
-  //   const post: iPost = {
-  //     id: uuid(),
-  //     title,
-  //     description,
-  //     status: PostStatus.PUBLIC,
-  //   };
-  //
-  //   this.posts.push(post);
-  //   return post; // 생성한 게시글 정보를 확인용으로 전달하기
-  // }
-  //
-
   async getPostById(id: number): Promise<Post> {
     const found = await this.postRepository.findOne({
       where: {
@@ -39,43 +19,4 @@ export class PostsService {
 
     return found;
   }
-
-  // getPostById(id: string): iPost {
-  //   const post = this.posts.find((post) => post.id === id);
-  //
-  //   if (!post) {
-  //     throw new NotFoundException(`Can't find Post. (id: ${id})`);
-  //   }
-  //
-  //   return post;
-  // }
-  //
-  // deletePostById(id: string): iDeletePostResponse {
-  //   const postToBeDeleted = this.getPostById(id);
-  //
-  //   this.posts = this.posts.filter((post) => post.id !== postToBeDeleted.id);
-  //
-  //   return {
-  //     statusCode: 200,
-  //     success: '게시글이 정상적으로 삭제되었습니다.',
-  //     deletedPost: postToBeDeleted,
-  //   };
-  // }
-  //
-  // updatePostStatus(id: string, status: PostStatus): iPatchPostResponse {
-  //   const post = this.getPostById(id);
-  //
-  //   if (!post) {
-  //     return {
-  //       error: '수정하려는 게시글이 존재하지 않습니다.',
-  //     };
-  //   }
-  //
-  //   post.status = status;
-  //
-  //   return {
-  //     success: '게시글이 성공적으로 수정되었습니다.',
-  //     updatedPost: post,
-  //   };
-  // }
 }
